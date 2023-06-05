587771801ce40495f13f770c531cb122c53a386a
MANICX100 <MANICX100@users.noreply.github.com>
2023-06-05 19:32:08 +0100

    Update non-oop.zig

--- non-oop.zig
+++ non-oop.zig
@@ -27,3 +27,6 @@ pub fn main() void {
     age_up(&dog);  // Increments Rex's age by 1
     print_age(dog);  // Prints: "Rex is 6 years old."
 }
+
+// In this version, the functions bark, age_up, and print_age are defined independently from the Dog struct, so they can be used in a more procedural manner. Also, note that for age_up we're passing a pointer to dog since it modifies the dog instance. The other two functions receive a copy of dog, as they don't modify the original instance.
+
